40470439b558e9de163595cafd56cff1586f9026
Ayush Mishra <mishraaayush078@gmail.com>
2025-10-05 22:24:40 +0530

    added

---
+++ src/ProgramNo_27.java
@@ -0,0 +1,23 @@
+/* 27. Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed.
+Then return the number of elements in nums which are not equal to val.
+ */
+
+public class ProgramNo_27 {
+    public static void main(String[] args){
+        int [] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        int ans= removeElement(nums,val);
+        System.out.println(ans);
+    }
+    public static int removeElement(int[] nums, int val){
+        int k=0;  // it store valid elements
+        for (int i=0;i< nums.length;i++){
+            if (nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
