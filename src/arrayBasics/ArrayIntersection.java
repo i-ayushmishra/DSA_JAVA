076352e3d3abe7c897b83bc56a7db4d07c8ed0af
Ayush Mishra <mishraaayush078@gmail.com>
2026-06-25 16:50:33 +0530

    commit

--- src/arrayBasics/ArrayIntersection.java
+++ src/arrayBasics/ArrayIntersection.java
@@ -15,7 +15,6 @@ public class ArrayIntersection {
 
     }
     public static void intersectElements(int [] arr1, int [] arr2 ){
-        int [] temp = new int[arr1.length];
         for (int i=0 ; i< arr1.length;i++){
             for (int j=0;j< arr2.length;j++){
                 if (arr1[i]==arr2[j]){
